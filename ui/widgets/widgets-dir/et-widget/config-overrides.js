a4b5a300071647c26d46fdb87ab4311980665fb0
GermanoGiudici <germano.giudici@gmail.com>
2021-11-23 15:30:52 +0100

    hot reload config

--- ui/widgets/widgets-dir/et-widget/config-overrides.js
+++ ui/widgets/widgets-dir/et-widget/config-overrides.js
@@ -1,23 +1,27 @@
 const appPackageJson = require('./package.json');
 const webpack = require('webpack');
+const path = require("path");
 
 module.exports = {
     webpack: function override(config, env) {
-        const isEnvDevelopment = env === 'development';
-        const isEnvProduction = env === 'production';
-
         const filename = `${appPackageJson.name}-${appPackageJson.version}`;
 
         const defaultOutput = config.output;
         config.plugins.push(new webpack.optimize.LimitChunkCountPlugin({
             maxChunks: 1
         }));
+        config.devtool = false;
+        config.plugins.push(new webpack.SourceMapDevToolPlugin({
+            filename: 'sourcemaps/[file].map',
+            publicPath: 'http://localhost:3000/',
+            namespace: filename,
+            moduleFilenameTemplate: 'webpack://[namespace]/[resource-path]?[loaders]',
+        }),);
 
         const entandoHRConfig = {
-            filename: isEnvDevelopment ? `static/js/${filename}.js` : `static/js/${filename}.hash[hash:8].js`,
+            filename: `static/js/${filename}.js`,
             libraryTarget: "umd",
             path: defaultOutput.path,
-            devtoolNamespace: filename,
             publicPath: defaultOutput.publicPath,
             jsonpFunction: `webpackJsonp${filename}`,
         }
